1e554ca5321cf02b72b00b8db35fa0c35b9a62f2
bmoren <ben@benmoren.com>
2020-02-10 14:11:06 -0600

    add playthrough mode

--- app_nue.js
+++ app_nue.js
@@ -7,15 +7,21 @@ var settings = {
 	totalClips  : 5,
 	pathToClips : "data/",
 	pbRate : 1,
-	mode : 'non-repeating-rand',
-
+	mode : 'random-playthrough',
 }
+
+let rpt = Array.from(Array(settings.totalClips), (e,i)=>i+1) //random playthrough generate array of numbers that can then be....
+shuffle(rpt) //shuffled for later choosing and playback.
+console.log(rpt);
+let rptTracker = 0;
+
+
 
 //init //press 'h' to hide the gui
 var gui = new dat.GUI();
 // gui.add(settings, 'changeThresh').name("Cut Thresh (sec)");
 gui.add(settings, 'totalClips').name("Total # of clips");
-gui.add(settings, 'mode', [ 'random', 'non-repeating-rand', 'linear' ] ).name("Playback Mode");
+gui.add(settings, 'mode', [ 'linear', 'random', 'non-repeating-rand', 'random-playthrough' ] ).name("Playback Mode");
 gui.add(settings, 'pathToClips').name("Clips Folder");
 // gui.add(settings, 'pbRate', 0,3).name("Playback Speed").step(0.25);
 
@@ -64,11 +70,11 @@ function toggle(element, pElement){
 	setTimeout(function(){
 		element.css('z-index', '500');
 		pElement.css('z-index', '0');
-		if(settings.mode == 'random'){
+		if(settings.mode === 'random'){
 			getRand = Math.floor(  Math.random() * settings.totalClips +1  )
 		}
 
-		if(settings.mode == 'linear'){
+		if(settings.mode === 'linear'){
 			if(linearTracker >= settings.totalClips){
 				linearTracker = 1;
 			}else{
@@ -79,13 +85,28 @@ function toggle(element, pElement){
 			console.log(linearTracker);
 		}
 
-		if(settings.mode == 'non-repeating-rand'){
+		if(settings.mode === 'non-repeating-rand'){
 			getRand = Math.floor(  Math.random() * settings.totalClips +1  )
 			while(getRand == pGetRand){ //are we the same, if so try again until we are not.
 				console.log("try again",getRand,pGetRand);
 				getRand = Math.floor(  Math.random() * settings.totalClips +1  )
 			}
 			pGetRand = getRand
+		}
+
+		if(settings.mode === 'random-playthrough'){
+
+		//move up by one
+		rptTracker ++
+
+		//reset
+		if(rptTracker === rpt.length){
+			shuffle(rpt)
+			rptTracker = 0;
+		}
+
+		getRand = rpt[rptTracker];
+
 		}
 
 		pElement.attr({ 'src': settings.pathToClips + getRand + '.mp4' });
@@ -95,4 +116,20 @@ function toggle(element, pElement){
 	//always keep the gui at the front :)
 	$('.dg').css('z-index', '9999');
 
+}
+
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
 }
